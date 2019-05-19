6f9a9a039cfe234788d54eee757b033b60833abb
tiwprabhat <prabhat_tiwari@outlook.com>
2019-05-19 09:27:42 +0530

    Added modified files

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -1,4 +1,24 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.Service.PostService;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+
+@Controller
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("posts")
+    public String getUserPosts(Model model) {
+        ArrayList<Post> posts = postService.getOnePost();
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
 }
